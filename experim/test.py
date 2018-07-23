b8ba72aba2a02f23a798b7c53eed23158544726a
bastamon <bastamon@sohu.com>
2018-07-23 10:26:31 +0800

    if 'test.py' in os.listdir(path='.'):

--- experim/test.py
+++ experim/test.py
@@ -1,6 +1,11 @@
 from com_filter1 import comfilter
+import os
 
-la = ["./experim/com_filter.py", "./experim/components1","./experim/components2",""]
+
+if 'test.py' in os.listdir(path='.'):
+    la = ["./com_filter.py", "./components1","./components2",""]
+else:
+    la = ["./experim/com_filter.py", "./experim/components1","./experim/components2",""]
 print(len(la))
 comfilter(la[1:])
 
